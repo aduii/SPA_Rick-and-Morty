3b3c41dfb6879b5bbe27dda4e8025fb53e9bf07d
aduii <alexjuep@hotmail.com>
2021-05-20 18:42:04 -0500

    First commit

---
+++ src/utils/resolveRoutes.js
@@ -0,0 +1,9 @@
+const resolve = (route) => {
+  if (route.length <= 3) {
+    let validateRoute = route === "/" ? route : "/:id";
+    return validateRoute;
+  }
+  return `/${route}`;
+};
+
+export default resolve;
